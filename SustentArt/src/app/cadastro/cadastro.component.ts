f8d30554d46a05a16f050848790a703896324973
Vanessa Marchetti <vanessa.b.marchetti@gmail.com>
2020-11-10 14:44:04 -0300

    atualizações

--- SustentArt/src/app/cadastro/cadastro.component.ts
+++ SustentArt/src/app/cadastro/cadastro.component.ts
@@ -28,7 +28,7 @@ export class CadastroComponent implements OnInit {
     if(this.senha === this.user.senha){
       this.authService.cadastrar(this.user).subscribe((resp: User) => {
         this.user = resp
-        this.router.navigate(['/login'])
+        this.router.navigate(['/home'])
         alert('Usuário cadastrado com sucesso!')
       })
     } else {
