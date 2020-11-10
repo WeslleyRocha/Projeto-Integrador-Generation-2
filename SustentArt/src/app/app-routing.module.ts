94f7a9a961cdd201339e9012056ab7c5fa163383
Weslley Rocha <weslley.nx@gmail.com>
2020-11-10 11:11:20 -0300

    service

--- SustentArt/src/app/app-routing.module.ts
+++ SustentArt/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
+import { HomeComponent } from './home/HomeComponent';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from "./home/HomeComponent";
+
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+
 
 const routes: Routes = [
   
   { path:'', redirectTo:'home', pathMatch:'full' },
   { path:'home', component: HomeComponent },
 
-  { path:'sobre-nos', component: SobreNosComponent }
+  { path:'sobre-nos', component: SobreNosComponent },
+
+  { path:'cadastro', component: CadastroComponent}
 
 ];
 
---
+++ SustentArt/src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+
+export class UserLogin
+{
+    
+ public email: string
+ public senha: string
+ public token: string
+
+}
