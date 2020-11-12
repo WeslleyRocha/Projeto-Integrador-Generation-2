00a86b5473147eac48d952637081f71ea1c996cc
Weslley Rocha <weslley.nx@gmail.com>
2020-11-12 16:53:35 -0300

    Atualização

--- SustentArt/src/app/app.module.ts
+++ SustentArt/src/app/app.module.ts
@@ -13,14 +13,11 @@ import { FeedComponent } from './feed/feed.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { PutPostagemComponent } from './put-postagem/put-postagem.component';
 import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
-=======
 import { PostTemaComponent } from './post-tema/post-tema.component';
 import { PutTemaComponent } from './put-tema/put-tema.component';
 import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
->>>>>>> put_post_Tema
 
 @NgModule({
   declarations: [
@@ -30,15 +27,12 @@ import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.componen
     FeedComponent,
     NavbarComponent,
     FooterComponent,
-<<<<<<< HEAD
     PutPostagemComponent,
     DeletePostagemComponent,
-    DeleteTemaComponent
-=======
+    DeleteTemaComponent,
     PostTemaComponent,
     PutTemaComponent,
     PerfilLateralComponent
->>>>>>> put_post_Tema
   ],
 
   imports: [
